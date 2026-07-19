92c504d00b46e04b1db210294f9ad7542876c360
Ashpin001 <153887034+Ashpin001@users.noreply.github.com>
2026-07-19 23:50:14 +0530

    Add files via upload

---
+++ wipro-jfs-course-main/wipro-jfs-course-main/wipro-jfs-course/LogicBuilding/src/L1/A4.java
@@ -0,0 +1,12 @@
+package L1;
+import java.util.Scanner;
+public class A4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("URK23CS1064");
+        System.out.print("Enter a string: ");
+		String str=s.nextLine();
+		str=str.replace(" ", "");
+		System.out.println("Length of the string is "+str.length());
+    }
+}
